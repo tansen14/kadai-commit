96f6ef36bf67d5cb4a5b4eaeef50a4a595ceb48d
SE.Selfish <s1B101402816@gmail.com>
2019-03-29 07:33:42 +0000

    update sample.php

--- sample.php
+++ sample.php
@@ -1,4 +1,7 @@
-CTYPE html>
+<?php
+    $result = '無し';
+?>
+<!DOCTYPE html>
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
@@ -6,10 +9,9 @@ CTYPE html>
     </head>
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果：無し</p>
+        <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
         <form action="sample.php" method="POST">
             <button type="submit" name="act" value="draw">おみくじをひく！</button>
         </form>
     </body>
 </html>
-
